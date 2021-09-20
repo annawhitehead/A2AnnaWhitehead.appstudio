0fff7b4a6ef9ab32901eb829b59d13f847caa809
annawhitehead <annawhitehead00@gmail.com>
2021-09-19 20:27:18 -0500

    Initial commit

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,24 @@
+/*
+// sets variables for prompts
+  let name = prompt(`Enter your first name`)
+  let userState= prompt(`Enter your abbreviated state`)
+  let weather= prompt(`Enter the temerature in Fahrenheit`)
+  let messages= ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+  
+  // displays message depending on prompt answers
+  if (weather < 32) {
+    console.log(`${name}, ${messages[0]}`)
+}
+  else if (weather >= 32 && weather <= 52 && userState == 'NE')  {
+    console.log(`${name}, ${messages[1]}`)
+  }
+  
+  else if (weather >= 32 && weather <= 52 && userState == 'FL')  {
+    console.log(`${name}, ${messages[2]}`)
+  }
+  
+    else if (weather >= 50 && weather <= 70 && userState == 'FL')  {
+      console.log(`${name}, ${messages[3]}`)
+  }
+  */
+  
---
+++ forms/switch/switch.js
@@ -0,0 +1,19 @@
+let name= prompt(`Enter your first name`)
+let userState= prompt(`Enter your abbreviated state`)
+let weather= prompt(`Enter the temerature in Fahrenheit`)
+let messages= ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+    
+switch (true) {
+case(weather < 32):
+   console.log(`${name}, ${messages[0]}`)
+break
+  case (weather >= 32 && weather <= 52 && userState == 'NE'):
+    console.log(`${name}, ${messages[1]}`)
+break
+  case(weather >= 32 && weather <= 52 && userState == 'FL') :
+    console.log(`${name}, ${messages[2]}`)
+break
+  case(weather >= 50 && weather <= 70 && userState == 'FL'):
+      console.log(`${name}, ${messages[3]}`)
+break
+}
