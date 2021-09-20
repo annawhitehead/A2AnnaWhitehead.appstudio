6b785b4e249aa0a521d3fa4c1b0c54dd008cef44
annawhitehead <annawhitehead00@gmail.com>
2021-09-19 20:28:59 -0500

    update

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -5,7 +5,7 @@
   let weather= prompt(`Enter the temerature in Fahrenheit`)
   let messages= ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
   
-  // displays message depending on prompt answers
+  // uses if else to display message depending on prompt answers
   if (weather < 32) {
     console.log(`${name}, ${messages[0]}`)
 }
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,8 +1,10 @@
+//sets prompt info for variables
 let name= prompt(`Enter your first name`)
 let userState= prompt(`Enter your abbreviated state`)
 let weather= prompt(`Enter the temerature in Fahrenheit`)
 let messages= ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
-    
+
+//uses switch for prompt answers    
 switch (true) {
 case(weather < 32):
    console.log(`${name}, ${messages[0]}`)
